534f8799879dbb76f95c3b277eb2aefe10a4a93e
bonchondev <benzay212@gmail.com>
2024-10-24 04:31:32 -0400

    lil cleaning up

--- views/src/pages/Index.svelte
+++ views/src/pages/Index.svelte
@@ -60,43 +60,43 @@
 {/snippet}
 
 {#snippet button(opts: ButtonOpts = {text: '', kind: '',size : "w-40 h-20"})}
-<button
-    onclick={()=>$sendJob.mutate(opts.kind)}
-    id={opts.kind}
-    disabled={$sendJob.isPending}
-    class={`
-      bg-blue-500
-      text-white
-      font-bold
-      ${opts.size}
-      rounded-lg
-      transition-all
-      duration-150
-      ease-in-out
-      ${
-          $sendJob.isPending
-              ? `bg-gray-400 text-gray-700 cursor-not-allowed`
-              : `border-b-4
-      border-blue-700
-      hover:bg-blue-600
-      hover:border-blue-600
-      active:border-blue-700
-      active:transform
-      active:translate-y-1
-      shadow-lg
-      hover:shadow-md
-      active:shadow-sm`
-      }
-    `}
->
-    <div class="flex flex-row items-center justify-center gap-x-2">
-        {#if $sendJob.isPending}
-                {@render spinner()}Loading...
-        {:else}
-                {opts.text}
-        {/if}
-    </div>
-</button>
+    <button
+        onclick={()=>$sendJob.mutate(opts.kind)}
+        id={opts.kind}
+        disabled={$sendJob.isPending}
+        class={`
+          bg-blue-500
+          text-white
+          font-bold
+          ${opts.size}
+          rounded-lg
+          transition-all
+          duration-150
+          ease-in-out
+          ${
+              $sendJob.isPending
+                  ? `bg-gray-400 text-gray-700 cursor-not-allowed`
+                  : `border-b-4
+          border-blue-700
+          hover:bg-blue-600
+          hover:border-blue-600
+          active:border-blue-700
+          active:transform
+          active:translate-y-1
+          shadow-lg
+          hover:shadow-md
+          active:shadow-sm`
+          }
+        `}
+    >
+        <div class="flex flex-row items-center justify-center gap-x-2">
+            {#if $sendJob.isPending}
+                    {@render spinner()}Loading...
+            {:else}
+                    {opts.text}
+            {/if}
+        </div>
+    </button>
 {/snippet}
 
 {#snippet result()}
